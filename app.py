c03afde8ec1e73f956cc243bfbd82f0577624f8e
JobScraper-git <jobscraper28@gmail.com>
2025-07-04 11:24:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/extract', methods=['POST'])
 def extract_redirect():
     data = request.get_json()
